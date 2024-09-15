b352185f9dcd2f6ec3b6bf0594dd6f4cafd7b0aa
Yannick Goossens <yannick@ziroh.be>
2024-09-15 18:17:13 +0200

    Use a service account instead

--- fountain.py
+++ fountain.py
@@ -11,7 +11,8 @@ import sys
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
+
+from google.oauth2 import service_account
 
 # If modifying these scopes, delete the file data/token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
@@ -142,20 +143,10 @@ def main():
     # The file data/token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('data/token.json'):
-        creds = Credentials.from_authorized_user_file(
-            'data/token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0, open_browser=False)
-        # Save the credentials for the next run
-        with open('data/token.json', 'w') as token:
-            token.write(creds.to_json())
+    if os.path.exists('credentials.json'):
+        creds = service_account.Credentials.from_service_account_file(
+            'credentials.json', scopes=SCOPES)
+
 
     service = build('sheets', 'v4', credentials=creds)
 
